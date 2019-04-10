91f47e5246597d65dee5e1834f8eef58fe23a2c4
Atruo <banyuls98@gmail.com>
2019-04-10 10:54:26 +0200

    anim

--- Patos/src/Animation.cpp
+++ Patos/src/Animation.cpp
@@ -14,6 +14,7 @@ Animation::Animation(Texture tex, int obj)
     Sprite *aux = new Sprite(anim_tex);
     Vector2i ventana(800,600);
     Vector2f posi = this->getRandomPosition(ventana);
+    int medida_sprite;
     if(obj == 0)//enemy
     {
          aux->setOrigin(0,0);
@@ -106,88 +107,90 @@ Animation::Animation(Texture tex, int obj)
 
     }else if(obj == 1)//player
     {
+        medida_sprite = 32;
         aux->setOrigin(0,0);
-        aux->setTextureRect(IntRect(24,30,24,24));
+        aux->setTextureRect(IntRect(24,30,50,50));
         aux->setPosition(400,300);
         aux->setScale(1.5,1.5);
-
-        aux->setTextureRect(IntRect(48,30,24,24));
+        //uu
+        int padding = 1;
+        aux->setTextureRect(IntRect(padding,padding*6+medida_sprite*6,medida_sprite,medida_sprite));
         matrix[0][0] = *aux;
-        aux->setTextureRect(IntRect(24,30,24,24));
+        aux->setTextureRect(IntRect(medida_sprite+padding,padding*6+medida_sprite*6,medida_sprite,medida_sprite));
         matrix[1][0] = *aux;
-        aux->setTextureRect(IntRect(48,30,24,24));
+        aux->setTextureRect(IntRect(medida_sprite*2+padding,padding,medida_sprite,medida_sprite));
         matrix[2][0] = *aux;
-        aux->setTextureRect(IntRect(0,30,24,24));
+        aux->setTextureRect(IntRect(padding,padding,medida_sprite,medida_sprite));
         matrix[3][0] = *aux;
         //ur
 
-        aux->setTextureRect(IntRect(28,200,24,24));
+        aux->setTextureRect(IntRect(28,200,medida_sprite,medida_sprite));
         matrix[0][1] = *aux;
-        aux->setTextureRect(IntRect(50,200,24,24));
+        aux->setTextureRect(IntRect(50,200,medida_sprite,medida_sprite));
         matrix[1][1] = *aux;
-        aux->setTextureRect(IntRect(28,200,24,24));
+        aux->setTextureRect(IntRect(28,200,medida_sprite,medida_sprite));
         matrix[2][1] = *aux;
-        aux->setTextureRect(IntRect(4,200,24,24));
+        aux->setTextureRect(IntRect(4,200,medida_sprite,medida_sprite));
         matrix[3][1] = *aux;
         //rr
 
-        aux->setTextureRect(IntRect(24,142,24,24));
+        aux->setTextureRect(IntRect(24,142,medida_sprite,medida_sprite));
         matrix[0][2] = *aux;
-        aux->setTextureRect(IntRect(51,142,24,24));
+        aux->setTextureRect(IntRect(51,142,medida_sprite,medida_sprite));
         matrix[1][2] = *aux;
-        aux->setTextureRect(IntRect(24,142,24,24));
+        aux->setTextureRect(IntRect(24,142,medida_sprite,medida_sprite));
         matrix[2][2] = *aux;
-        aux->setTextureRect(IntRect(0,142,24,24));
+        aux->setTextureRect(IntRect(0,142,medida_sprite,medida_sprite));
         matrix[3][2] = *aux;
         //rd
 
-        aux->setTextureRect(IntRect(24,170,24,24));
+        aux->setTextureRect(IntRect(24,170,medida_sprite,medida_sprite));
         matrix[0][3] = *aux;
-        aux->setTextureRect(IntRect(48,170,24,24));
+        aux->setTextureRect(IntRect(48,170,medida_sprite,medida_sprite));
         matrix[1][3] = *aux;
-        aux->setTextureRect(IntRect(24,170,24,24));
+        aux->setTextureRect(IntRect(24,170,medida_sprite,medida_sprite));
         matrix[2][3] = *aux;
-        aux->setTextureRect(IntRect(0,170,24,24));
+        aux->setTextureRect(IntRect(0,170,medida_sprite,medida_sprite));
         matrix[3][3] = *aux;
         //dd
 
-        aux->setTextureRect(IntRect(24,0,24,24));
-        matrix[0][4] = *aux;
-        aux->setTextureRect(IntRect(48,0,24,24));
-        matrix[1][4] = *aux;
-        aux->setTextureRect(IntRect(24,0,24,24));
-        matrix[2][4] = *aux;
-        aux->setTextureRect(IntRect(0,0,24,24));
-        matrix[3][4] = *aux;
+        aux->setTextureRect(IntRect(padding,padding,medida_sprite,medida_sprite));
+        matrix[0][0] = *aux;
+        aux->setTextureRect(IntRect(32+padding,padding,medida_sprite,medida_sprite));
+        matrix[1][0] = *aux;
+        aux->setTextureRect(IntRect(64+padding,padding,medida_sprite,medida_sprite));
+        matrix[2][0] = *aux;
+        aux->setTextureRect(IntRect(padding,padding,medida_sprite,medida_sprite));
+        matrix[3][0] = *aux;
         //dl
 
-        aux->setTextureRect(IntRect(24,86,24,24));
+        aux->setTextureRect(IntRect(24,86,medida_sprite,medida_sprite));
         matrix[0][5] = *aux;
-        aux->setTextureRect(IntRect(52,86,24,24));
+        aux->setTextureRect(IntRect(52,86,medida_sprite,medida_sprite));
         matrix[1][5] = *aux;
-        aux->setTextureRect(IntRect(24,86,24,24));
+        aux->setTextureRect(IntRect(24,86,medida_sprite,medida_sprite));
         matrix[2][5] = *aux;
-        aux->setTextureRect(IntRect(0,86,24,24));
+        aux->setTextureRect(IntRect(0,86,medida_sprite,medida_sprite));
         matrix[3][5] = *aux;
         //ll
 
-        aux->setTextureRect(IntRect(24,58,24,24));
+        aux->setTextureRect(IntRect(24,58,medida_sprite,medida_sprite));
         matrix[0][6] = *aux;
-        aux->setTextureRect(IntRect(48,58,24,24));
+        aux->setTextureRect(IntRect(48,58,medida_sprite,medida_sprite));
         matrix[1][6] = *aux;
-        aux->setTextureRect(IntRect(24,58,24,24));
+        aux->setTextureRect(IntRect(24,58,medida_sprite,medida_sprite));
         matrix[2][6] = *aux;
-        aux->setTextureRect(IntRect(0,58,24,24));
+        aux->setTextureRect(IntRect(0,58,medida_sprite,medida_sprite));
         matrix[3][6] = *aux;
         //lu
 
-        aux->setTextureRect(IntRect(28,116,24,24));
+        aux->setTextureRect(IntRect(28,116,medida_sprite,medida_sprite));
         matrix[0][7] = *aux;
-        aux->setTextureRect(IntRect(51,116,24,24));
+        aux->setTextureRect(IntRect(51,116,medida_sprite,medida_sprite));
         matrix[1][7] = *aux;
-        aux->setTextureRect(IntRect(28,116,24,24));
+        aux->setTextureRect(IntRect(28,116,medida_sprite,medida_sprite));
         matrix[2][7] = *aux;
-        aux->setTextureRect(IntRect(4,116,24,24));
+        aux->setTextureRect(IntRect(4,116,medida_sprite,medida_sprite));
         matrix[3][7] = *aux;
 
         spr = new Sprite(matrix[pos][col]);
